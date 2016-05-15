53b97a0d9f68cd94231f57fe14fb6ba245756a3e
patriciobustos <patricio.b@manguito.com.mx>
2016-05-15 12:33:11 -0500

    ultimas actualizaciones

--- src/app/components/cleanbar/cleanbar.directive.js
+++ src/app/components/cleanbar/cleanbar.directive.js
@@ -1,30 +1,12 @@
 (function() {
   'use strict';
-
   angular
     .module('app2')
-    .directive('cleanBar', cleanBar);
-
-  /** @ngInject */
-  function cleanBar() {
+    .directive('cleanbar', cleanbar);
+  function cleanbar() {
     var directive = {
-      restrict: 'E',
       templateUrl: 'app/components/cleanbar/cleanbar.html',
-      scope: {
-          creationDate: '='
-      },
-      controller: CleanbarController
     };
-
     return directive;
-
-    /** @ngInject */
-    function CleanbarController(moment) {
-      var vm = this;
-
-      // "vm.creationDate" is available by directive option "bindToController: true"
-      vm.relativeDate = moment(vm.creationDate).fromNow();
-    }
   }
-
 })();
--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -1,32 +1,12 @@
 (function() {
   'use strict';
-
   angular
     .module('app2')
     .directive('topNavbar', topNavbar);
-
-  /** @ngInject */
   function topNavbar() {
     var directive = {
-      restrict: 'E',
       templateUrl: 'app/components/navbar/navbar.html',
-      scope: {
-          creationDate: '='
-      },
-      controller: NavbarController,
-      controllerAs: 'vm',
-      bindToController: true
     };
-
     return directive;
-
-    /** @ngInject */
-    function NavbarController(moment) {
-      var vm = this;
-
-      // "vm.creationDate" is available by directive option "bindToController: true"
-      vm.relativeDate = moment(vm.creationDate).fromNow();
-    }
   }
-
 })();
